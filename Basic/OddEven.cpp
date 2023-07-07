99973806e8ef3b5e985429f548f8c6e024126b10
sachin shakya <sachinshakya205001@gmail.com>
2023-07-07 08:03:23 +0530

    OddEven

---
+++ Basic/OddEven.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+int num=0;
+cout<<"Enter the number to check wheather it is even or odd :"<<endl;
+cin>>num;
+ if (num%2==0){
+    cout<<"Number is even "<<num<<endl;
+ }else{
+    cout<<"Number is Odd "<<num<<endl;
+ }
+    return 0;
+}
