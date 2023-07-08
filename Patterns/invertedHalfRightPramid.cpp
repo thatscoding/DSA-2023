4df77d8def650637ad665ef7bf4c900022050c9a
sachin shakya <sachinshakya205001@gmail.com>
2023-07-08 13:39:17 +0530

    added basic pattern problems

---
+++ Patterns/invertedHalfRightPramid.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number :"<<endl;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=n-i+1; j>=1; j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
