870cf80e3c4d3564f6d6a1f34b6f28e32372c312
sachin shakya <sachinshakya205001@gmail.com>
2023-07-20 08:12:36 +0530

    fix : tree & feat : traversal

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -23,35 +23,70 @@ public:
     }
 };
 
-class Btree
-{
-    Node *root;
-
-public:
-    Btree()
-    {
-        this->root = NULL;
-    }
-    int createNode();
-    void deleteNode();
-    void inOrder();
-    void preOrder();
-    void postOrder();
-};
-
-int Btree::createNode()
+Node *create()
 {
     int x;
-    cout << "Enter the data , (for exiting enter -1)" << endl;
+    cout << "enter the data : you wnant to exit enter -1" << endl;
     cin >> x;
-    Node *newNode = new Node(x);
-
     if (x == -1)
     {
         return NULL;
     }
+    Node *newNode = new Node(x);
+
+    cout << "enter the left child of " << x << endl;
+    newNode->left = create();
+
+    cout << "enter the right child of " << x << endl;
+    newNode->right = create();
+
+    return newNode;
+}
+
+void inOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void preOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void postOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
 }
 
 int main()
 {
+    Node *root = NULL;
+    root = create();
+
+    cout << endl
+         << "InOrder traversal :" << endl;
+    inOrder(root);
+    cout << endl
+         << "Pre Order Traversal " << endl;
+    preOrder(root);
+    cout << endl
+         << "Post Order Traversal " << endl;
+    postOrder(root);
 }
